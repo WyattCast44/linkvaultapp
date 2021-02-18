b414c08369a7d031aea8ed86c636723ed204e726
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-18 14:29:58 -0800

    wip

--- app/Http/Livewire/LinkShow.php
+++ app/Http/Livewire/LinkShow.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Link;
 use Livewire\Component;
 use Illuminate\Support\Str;
 
--- resources/views/livewire/components/modals/command-palette.blade.php
+++ resources/views/livewire/components/modals/command-palette.blade.php
@@ -26,8 +26,9 @@
         <div 
             role="dialog" 
             aria-modal="true"
-            aria-labelledby="modal-headline" 
             x-show="showCommandPalette"
+            aria-labelledby="modal-headline" 
+            x-on:click.away="showCommandPalette = false"
             x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
