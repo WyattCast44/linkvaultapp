456f8459243637b507e7adf5f5e1cb9ea68474ac
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-18 12:58:02 -0800

    wip

--- resources/views/livewire/components/inputs/create-link.blade.php
+++ resources/views/livewire/components/inputs/create-link.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data x-on:keydown.escape="document.activeElement.blur()">
     <label for="newLink" class="relative">
         <span class="absolute left-0 flex items-center justify-center px-2" style="top:0px">
             <x-icon-plus class="w-6 h-6 text-gray-400" />
@@ -11,7 +11,7 @@
             placeholder="Save a URL"
             wire:model="newLinkUrl"
             wire:keydown.enter="createLink"
-            class="w-full pl-10 border border-gray-300 bg-gray-50">
+            class="w-full pl-10 border border-t-0 border-b-0 border-gray-300 bg-gray-50">
     </label>
 </div>
 
