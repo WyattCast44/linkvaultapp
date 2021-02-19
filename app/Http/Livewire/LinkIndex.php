3eb7e3622d33d174df64a80788606faf676867fd
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-19 08:43:41 -0800

    wip

--- app/Http/Livewire/LinkIndex.php
+++ app/Http/Livewire/LinkIndex.php
@@ -14,7 +14,6 @@ class LinkIndex extends Component
 
     protected $listeners = [
         'linkAdded' => 'updateLinks',
-        'move-focus' => 'moveFocusToNextLink'
     ];
 
     public function mount()
@@ -25,25 +24,6 @@ class LinkIndex extends Component
     public function updateLinks()
     {
         $this->links = auth()->user()->refresh()->links;
-    }
-
-    public function moveFocusToNextLink()
-    {
-        if ($this->activeLink == null) {
-            $this->activeLink = $this->links->first();
-        } else {
-            $keys = collect($this->links->modelKeys());
-
-            $index = $keys->search($this->activeLink->id);
-
-            if (array_key_exists($index + 1, $keys->toArray())) {
-                $this->activeLink = $this->links->find($keys[$index + 1]);
-            } else {
-                $this->activeLink = $this->links->first();
-            }
-        }
-
-        $this->emit('move-focus-to-next-link', "#link-id-" . $this->activeLink->hash_id);
     }
 
     public function deleteLink($link)
