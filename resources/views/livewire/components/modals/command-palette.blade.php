585a9fb514b7e197f00df4cd9d658bdfa93de09f
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-18 16:29:25 -0800

    wip

--- resources/views/livewire/components/modals/command-palette.blade.php
+++ resources/views/livewire/components/modals/command-palette.blade.php
@@ -35,10 +35,12 @@
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            class="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+            class="inline-block px-4 pt-3 pb-3 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-4">
 
 
             <label for="command-palette-input">
+
+                <p class="mb-2 text-sm font-semibold text-gray-600">Command Palette</p>
 
                 <input type="text" name="command" id="command-palette-input" list="commands" class="w-full" x-on:keydown.enter="parseCommandArgs()">
 
@@ -51,31 +53,6 @@
                   </datalist>
 
             </label>
-
-            {{-- <div>
-                <div class="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full">
-                    <!-- Heroicon name: outline/check -->
-                    <svg class="w-6 h-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                    </svg>
-                </div>
-                <div class="mt-3 text-center sm:mt-5">
-                    <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-headline">
-                        Payment successful
-                    </h3>
-                    <div class="mt-2">
-                        <p class="text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="mt-5 sm:mt-6">
-                <button type="button" class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
-                    Go back to dashboard
-                </button>
-            </div> --}}
 
         </div>
 
