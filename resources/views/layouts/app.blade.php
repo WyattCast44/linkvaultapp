d6624bf50e3561bdd74b42944f8b049adc7c4e33
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-18 17:38:16 -0800

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,37 +2,46 @@
 
 @section('body')
 
-    <div class="py-2 text-center bg-white border-b">
-        <div class="flex items-center justify-center space-x-2">
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit" class="btn-link">Logout</button>
-            </form>    
+    <header class="sticky top-0">
+        
+        <div class="py-2 text-center bg-white border-b">
+            <div class="flex items-center justify-between max-w-3xl px-3 mx-auto space-x-2">
+                <h1>
+                    <a href="{{ route('dashboard') }}" class="font-mono font-semibold hover:no-underline">
+                        Link Vault 🔒
+                    </a>
+                </h1>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn-link">Logout</button>
+                </form>    
+            </div>
         </div>
-    </div>
 
-    <div class="bg-white border-b">
-        <div class="max-w-3xl mx-auto">
-            <livewire:components.inputs.create-link />
+        <div class="bg-white border-b">
+            <div class="max-w-3xl mx-auto">
+                <livewire:components.inputs.create-link />
+            </div>
         </div>
-    </div>
 
-    <div class="mb-2 text-center bg-white border-b">
-        <div class="flex items-center justify-between max-w-3xl mx-auto border-l border-r divide-x">
-            <a href="{{ route('dashboard') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
-                Dashboard
-            </a>
-            <a href="{{ route('dashboard.links.index') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
-                Links
-            </a>
-            <a href="{{ route('dashboard.tags.index') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
-                Tags
-            </a>
+        <div class="mb-2 text-center bg-white border-b">
+            <div class="flex items-center justify-between max-w-3xl mx-auto border-l border-r divide-x">
+                <a href="{{ route('dashboard') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
+                    Dashboard
+                </a>
+                <a href="{{ route('dashboard.links.index') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
+                    Links
+                </a>
+                <a href="{{ route('dashboard.tags.index') }}" class="z-0 w-full py-2 text-center hover:bg-gray-200 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-600">
+                    Tags
+                </a>
+            </div>
         </div>
-    </div>
+        
+    </header>
     
     <div class="max-w-3xl mx-auto">
         @yield('content')
     </div>
-    
+
 @endsection
