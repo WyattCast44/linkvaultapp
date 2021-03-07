37227dfcb45f46f24dc3d2c1d4565f336198a8a8
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-03-07 12:47:03 -0800

    wip

--- resources/views/dashboard/tags/show.blade.php
+++ resources/views/dashboard/tags/show.blade.php
@@ -8,12 +8,13 @@
 
         <ul class="mb-8 bg-white border divide-y">
             @forelse ($links as $link)
-                <li class="flex items-center justify-between p-3">
+                @include('dashboard.links.partials.link')
+                {{-- <li class="flex items-center justify-between p-3">
                     <a href="{{ $link->url }}" target="_blank" rel="noopener">{{ $link->url }}</a>
                     <button wire:click="unlinkTag({{ $link->id }})" class="flex items-center justify-center" title="Remove tag from link">
                         <x-icon-backspace class="w-5 h-5 text-gray-500 hover:text-gray-700" />
                     </button>
-                </li>
+                </li> --}}
             @empty
                 <li class="p-3">
                     No links yet
