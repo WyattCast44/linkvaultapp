2c2b2ef99faaa58a1756a09db61999bce3b66a4e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-03-07 22:10:33 -0800

    wip

--- resources/views/dashboard/search/index.blade.php
+++ resources/views/dashboard/search/index.blade.php
@@ -21,25 +21,26 @@
 
         @if ($tags->count() > 0)
             
-        <div class="flex flex-wrap my-5 space-x-1 space-y-1">
-            @foreach ($tags as $tag)
-                <button type="button" class="flex items-center justify-center px-2 py-1 text-sm transition duration-100 bg-white border rounded hover:no-underline hover:bg-indigo-300 whitespace-nowrap" title="Apply tag filter" wire:click="toggleTagFilter({{ $tag->id }})">
-                    {{ $tag->name }}
+            <div class="flex flex-wrap my-5">
+                @foreach ($tags as $tag)
+                    <button type="button" class="flex items-center justify-center px-2 py-1 mb-1 mr-1 text-sm transition duration-100 bg-white border rounded hover:no-underline hover:bg-indigo-300 whitespace-nowrap" title="Apply tag filter" wire:click="toggleTagFilter({{ $tag->id }})">
+                        {{ $tag->name }}
 
-                    <div class="flex items-center justify-center ml-1">
-                        @if (in_array($tag->slug, $activeTags))
-                            <x-icon-x class="w-3 h-3" />
-                        @endif
-                    </div>
+                        <div class="flex items-center justify-center ml-1">
+                            @if (in_array($tag->slug, $activeTags))
+                                <x-icon-x class="w-3 h-3" />
+                            @endif
+                        </div>
 
-                </button>
-            @endforeach
-        </div>
+                    </button>
+                @endforeach
+            </div>
+            
         @endif
 
     </div>
 
-    <div class="max-w-3xl mx-4 my-10 lg:my-16 lg:mx-auto">
+    <div class="max-w-3xl mx-4 my-10 lg:mx-auto">
 
         @if ($results)
             
